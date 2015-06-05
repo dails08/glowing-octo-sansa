a567ab32baef46a183c33289893cb5f475083e22
Chris Dailey <christopher.dailey@gmail.com>
2015-06-04 20:33:29 -0400

    Fixed import bug in downloader

--- downloader.py
+++ downloader.py
@@ -1,7 +1,8 @@
 import urllib2
+import sys
 
 
-for i in range(arg[1], arg[2]):
+for i in range(sys.args[1], sys.args[2]):
     print "downloading "+str(i)+"..."
     url = "http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-" + str(i) + ".csv.zip"
     f = urllib2.urlopen(url)
