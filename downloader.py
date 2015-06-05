b98befd225938fa5727fea5d930fb7705b4004aa
Chris Dailey <christopher.dailey@gmail.com>
2015-06-04 20:31:07 -0400

    Fixed bug in url in downloader

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import urllib2
 
 for i in range(arg[1], arg[2]):
     print "downloading "+str(i)+"..."
-    url = "http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-" + str(0) + ".csv.zip"
+    url = "http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-" + str(i) + ".csv.zip"
     f = urllib2.urlopen(url)
     with open("/gpfs/gpfsfpo/"+str(i) + ".zip", "wb") as filename:
         filename.write(f.read())
