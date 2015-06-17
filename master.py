15cbf6779286876ad941bab6b7b9b676dc30630a
Chris Dailey <christopher.dailey@gmail.com>
2015-06-17 16:41:12 -0400

    testing

--- master.py
+++ master.py
@@ -5,6 +5,9 @@ def master():
 	context = zmq.Context()
 	publisher = context.socket(zmq.PUB)
 	publisher.bind("tcp://*:5557")
+	
+	puller = context.socket(zmq.PULL)
+	puller.bind("tcp://localhost:5559")
 	for i in range(1000):
 		print "Sending message "+str(i)
 		publisher.send("This is message "+ str(i))
--- worker.py
+++ worker.py
@@ -8,6 +8,10 @@ def worker():
 	receiver = context.socket(zmq.SUB)
 	receiver.connect("tcp://10.122.102.45:5557")
 	receiver.setsockopt(zmq.SUBSCRIBE,'')
+	
+	pusher = context.socket(zmq.PUSH)
+	pusher.connect("tcp://10.122.102.45:5559")
+	
 	while True:
 		print "Listening..."
 		msg = receiver.recv()
