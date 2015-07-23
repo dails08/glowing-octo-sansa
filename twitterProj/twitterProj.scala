86673f5d7a358f648587ad6af18cc2bf235158f8
root <root@spark1.dails.com>
2015-07-22 20:05:11 -0500

    Progress

--- twitterProj/twitterProj.scala
+++ twitterProj/twitterProj.scala
@@ -21,29 +21,68 @@ object Twitter {
     val sparkConf = new SparkConf().setAppName("twitterProj").setMaster("spark://spark1:7077")
     val ssc = new StreamingContext(sparkConf, Seconds(5))
     val stream = TwitterUtils.createStream(ssc, None)
+
     println("Type of stream: " + stream.getClass)
     val tweets = stream.map(tweet => tweet.getText())
     println("Type of tweets: " + tweets.getClass)
-    val tags = stream.flatMap(tweet => tweet.getText().split(" ")).filter(word => word.startsWith("#"))
-    println("Type of tags: " + tags.getClass)
-    val sortedCounts = tags.map(tag => (tag, 1))
-                     .reduceByKey((a, b) => a + b)
-                     .map(count => (count._2, count._1))
-                     .transform(rdd => rdd.sortByKey(false))
+    val sortedTags = stream.flatMap(tweet => 
+                      tweet.getText().split(" ")
+                      .filter(word => word.startsWith("#"))
+                      .map(tag => (tag, 1)))
+                      .reduceByKey((a, b) => a + b)
+                      .map(count => (count._2, count._1))
+                      .transform(rdd => rdd.sortByKey(false))
+                    //  .take(numTopics.toInt)
+                    //  .transform(tup => tup._2)
 
-    println("Type of sortedCounts: " + sortedCounts.getClass)
-    //println("Type of sortedCounts[T]: " + sortedCounts[T])
-    //sortedCounts.foreach(rdd => println("\nTop "+numTopics+" tags:\n" + rdd.take(numTopics.toInt).mkString("\n")))
-    sortedCounts.foreach(rdd => {
-        val pops = rdd.take(numTopics.toInt)
-        //authorTagLists = stream.map(tweet => (tweet.getUser().getName(), tweet.getText().split(" ").filter(word => word.startsWith("#"))
-        val popAuthors = stream.map(tweet => 
-                tweet.getText().split(" "))
-               .filter(word => word.startsWith("#"))
-               //.intersection(pops)
-               .map(tag => (tag, tweet.getUser().getName()))
-               .foreach(tup => print(tup))
-    })
+    val popTags = sortedTags.take(numTopics.toInt)     
+
+//    val tags = stream.flatMap(tweet => tweet.getText().split(" ")).filter(word => word.startsWith("#"))
+//    println("Type of tags: " + tags.getClass)
+
+//    val sortedCounts = tags.map(tag => (tag, 1))
+//                     .reduceByKey((a, b) => a + b)
+//                     .map(count => (count._2, count._1))
+//                     .transform(rdd => rdd.sortByKey(false))
+
+//    println("Type of sortedCounts: " + sortedCounts.getClass)
+    
+    
+    popTags.foreach(tag => {
+        println("Starting inside popTags")
+        println("Length of popTags: "+popTags.count)
+        println("Length of rdd: "+rdd.count)
+        println("Length of popTags: " + popTags.length)
+        println("For tag: " + tag)
+        stream.filter(tweet => tweet.getText().split(" ").startsWith("#"))
+              .foreach(tweet => println("\t" + tweet.getUser().getName()))
+        })
+
+//            stream.map(tweet => 
+//            tweet.getText().split(" ")
+//            .filter(word => word.startsWith("#"))
+//            .filter(word => pops.contains(word))
+//            .map(tag => {
+//                          println("Keeping "+tweet.getUser().getName() + " and " + tag)
+//                          (tweet.getUser().getName(), tag)
+//                        }))
+                       
+            //.filter(tup => pops.contains(tup._2))
+//            .foreach(tup => println((tweet.getUser().getName(), tup))))
+        //val innerTags = 
+        //stream.map(tweet => {
+        //        //var userName = tweet.getUser().getName()
+        //        tweet.getText().split(" ")
+        //        .filter(word => word.startsWith("#"))
+        //        .filter(word => pops.contains(word))
+        //        .foreach(word => println(word, tweet.getUser().getName()))
+        //        })
+        //        //.intersection(pops)
+        //println("Length of innerTags: "+innerTags.count)               
+        //val authorTagTuples = innerTags.map(tag => (tag, tweet.getUser().getName()))
+        //println("Length of authorTagTuples: "+authorTagTuples.count)       
+        //authorTagTuples.foreach(tup => print(tup))
+//    })
 
 
     ssc.start()
