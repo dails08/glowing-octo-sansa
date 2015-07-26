aaacf0527cb1283d6ebc3b8654f1de4be0398cd3
root <root@hw41.dails.com>
2015-07-26 16:27:44 -0500

    Progress

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ for i in range(int(sys.argv[1]), int(sys.argv[2])):
     print "downloading "+str(i)+"..."
     url = "http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-2gram-20090715-" + str(i) + ".csv.zip"
     f = urllib2.urlopen(url)
-    with open("/gpfs/gpfsfpo/"+str(i) + ".zip", "wb") as filename:
+    with open("/gpfs/gpfsfpo/gpfs"+ sys.argv[3]+"/"+str(i) + ".zip", "wb") as filename:
         filename.write(f.read())
 
 
